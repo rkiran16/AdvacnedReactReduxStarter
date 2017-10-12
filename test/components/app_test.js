b2d47762c16ae9e4f7896091e4878484fe262786
Ravi Kanculakunta <rkanculakunta@resourceammirati.com>
2017-10-12 10:14:36 -0400

    update
    update

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,6 +1,7 @@
 import { renderComponent , expect } from '../test_helper';
 import App from '../../src/components/app';
 
+// Use 'describe' to group together similar tests
 describe('App' , () => {
   let component;
 
@@ -8,7 +9,9 @@ describe('App' , () => {
     component = renderComponent(App);
   });
 
+  // use 'it' to test a single attribute of target
   it('renders something', () => {
-    expect(component).to.exist;
+    //use 'expect' to make an assertion about a target
+    expect(component).to.contain('React simple starter');
   });
 });
